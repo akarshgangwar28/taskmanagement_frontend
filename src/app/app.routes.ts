1f83be358a9bbae59aeddfaaa61b1fe9a18d4449
akarshgangwar28 <akarshprog28@gmail.com>
2026-05-01 10:11:19 +0530

    feat: complete base application setup and layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Layout } from './shared/layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      // Routes like login, register, tasks to be added here
+    ],
+  },
+];
